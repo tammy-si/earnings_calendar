0fec9f19e0e7af7db73a223b9d48627ec43174c9
tammy-si <sitammy6@gmail.com>
2024-02-28 14:27:58 -0800

    Fixed some stuff

--- server/stocks.js
+++ server/stocks.js
@@ -5,6 +5,9 @@ const Stock = require("../models/stockModel");
 const Week = require("../models/weekModel");
 const ObjectId = require("mongodb").ObjectId;
 const { connectDB } = require("../helpers/connect.js");
+const cors = require("cors");
+
+app.use(cors());
 
 /* get all the weeks and their start dates */
 app.get("/", async (req, res) => {
@@ -12,9 +15,7 @@ app.get("/", async (req, res) => {
   await connectDB();
   try {
     const weeks = await Week.find({}).exec();
-    console.log("after");
-    console.log(weeks);
-    return res.json("hi");
+    return res.json(weeks);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
